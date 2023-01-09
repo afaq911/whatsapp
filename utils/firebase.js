f2e1c326e3c7f5934f78f51aaf812289d4913862
afaq911 <94537533+afaq911@users.noreply.github.com>
2023-01-10 00:23:44 +0500

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB0PkVwAFnuBzbJ_3h6v1XbEfwbYdVaKO0',
-  authDomain: 'chit-chat-705cd.firebaseapp.com',
-  projectId: 'chit-chat-705cd',
-  storageBucket: 'chit-chat-705cd.appspot.com',
-  messagingSenderId: '436999790326',
-  appId: '1:436999790326:web:dac92baf4d8721a99b96d7',
-  measurementId: 'G-9QFJ13S7XH',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket:process.env.STORAGE,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.M_ID,
 };
 
 // Initialize Firebase
